49f2e061746fee8db188575f0d6b44e3a927e003
MortdaLtfe <131448577+MortdaLtfe@users.noreply.github.com>
2023-12-19 23:09:10 +0300

    Update db.php

--- database/db.php
+++ database/db.php
@@ -3,6 +3,6 @@
   $user = 'root';
   $pass = 'root';
   $DB = 'login';
-  // $conn = new PDO("mysql:host=$host;dbName=$DB",$user,$pass); 
+  
  $conn = mysqli_connect( $host, $user, $pass, $DB) or die('Error Connecting');
 ?>
